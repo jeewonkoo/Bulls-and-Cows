2924fc34089925d01de9084e572d25a41fe1a408
jeewonkoo <jeewonkoo@gmail.com>
2023-02-08 04:40:34 -0600

    create board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const TILE_LENGTH = 4;
+
 function App() {
+  
+  const [solution, setSolution] = useState(0);
+  const [guesses, setGuesses] = useState(Array(6).fill(null));
+
+  useEffect(() => {
+
+  }, []);
+
+  const generateSolution = () => {
+    const random = Math.floor(Math.random() * 9000 + 1000);
+    setSolution(random);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>BULLS && COWS</h1>
+      <button
+        onClick={()=>generateSolution()}
         >
-          Learn React
-        </a>
-      </header>
+        GAME START
+      </button>
+      <div className='board'>
+        {
+          guesses.map((guess) => {
+            return (
+              <Line guess={guess ?? ''}/>
+            );
+          })
+        }
+      </div>
     </div>
   );
 }
+
+function Line({guess}) {
+  const tiles = [];
+  for (let i=0; i<TILE_LENGTH; i++) {
+    const num = guess[i]
+    tiles.push(
+      <div key={i} className="tile">
+        {num}
+      </div>
+    );
+  }
+  return (
+    <div className='line'>
+      {tiles}
+    </div>
+  )
+}
+
 
 export default App;
